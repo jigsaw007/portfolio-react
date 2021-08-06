16dc2c2a77c250ca8a75d87e89af17a4761bc1b7
Anuj <fplnepal@gmail.com>
2021-08-06 09:57:12 +0900

    updated about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -27,7 +27,7 @@ function About() {
                                      :) </span>
                                     <br />
                                     <br />
-                                    👋 I'm Anuj Joshi. Fictional person for preview purposes :) I'm working with newest front-end frameworks like Angular, React and Vue. What you are seeing now is portfolio template from Dorota1997. If you like this portfolio template, make sure to ⭐ the repository to make it more recognizable for other users. Thank you 💜
+                                    👋 Front End Developer / Wordpress Expert / Currently working as System Engineer in Tokyo.
                                 </div>
                             </div>
                         </div>
